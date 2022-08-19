0a1263b1a0d567dd760d35fbe8f567d52fcc8fee
Alexander Predeus <ap41@sanger.ac.uk>
2022-08-19 22:52:34 +0100

    Update solo_QC.sh - include species

--- scripts/solo_QC.sh
+++ scripts/solo_QC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-echo -e "Sample\tRd_all\tRd_in_cells\tFrc_in_cells\tUMI_in_cells\tCells\tMed_nFeature\tGood_BC\tPaired\tStrand\tall_u+m\tall_u\texon_u+m\texon_u\tfull_u+m\tfull_u"
+echo -e "Sample\tRd_all\tRd_in_cells\tFrc_in_cells\tUMI_in_cells\tCells\tMed_nFeature\tGood_BC\tSpecies\tPaired\tStrand\tall_u+m\tall_u\texon_u+m\texon_u\tfull_u+m\tfull_u"
 
 
 for i in *
@@ -12,6 +12,16 @@ do
     then
       PAIRED="Paired"
     fi
+
+    REF="Other"
+    if [[ `grep "^genomeDir" $i/Log.out | tail -n1 | grep "/human/"` != "" ]]
+    then
+      REF="Human"
+    elif [[ `grep "^genomeDir" $i/Log.out | tail -n1 | grep "/mouse/"` != "" ]]
+    then
+      REF="Mouse"
+    fi
+
     R1=`grep "Number of Reads," $i/output/Gene/Summary.csv | awk -F "," '{print $2}'`
     B=`grep "Reads With Valid Barcodes," $i/output/Gene/Summary.csv | awk -F "," '{print $2}'`
     G1=`grep "Reads Mapped to Genome: Unique+Multiple," $i/output/Gene/Summary.csv | awk -F "," '{print $2}'`
@@ -26,13 +36,16 @@ do
     R3=`grep "UMIs in Cells," $i/output/GeneFull/Summary.csv | awk -F "," '{print $2}'`
     GC=`grep "Median GeneFull per Cell," $i/output/GeneFull/Summary.csv | awk -F "," '{print $2}'`
     ST=`grep "^soloStrand" $i/Log.out | grep RE-DEFINED | awk '{print $2}'`
+
     if [[ $ST == "" ]]
     then
       ST="Undef"
     elif [[ $PAIRED == "Paired" ]]
     then
+      ## only 5' experiments can be processed as paired-end; however, actual STAR command has "--soloStrand Forward"
+      ## since read order for PE processing is R1 R2 (it's R2 R1 for regular single-end 10X)
       ST="Reverse"
     fi
-    echo -e "$i\t$R1\t$R2\t$CF\t$R3\t$C\t$GC\t$B\t$PAIRED\t$ST\t$G1\t$G2\t$E1\t$E2\t$F1\t$F2"
+    echo -e "$i\t$R1\t$R2\t$CF\t$R3\t$C\t$GC\t$B\t$REF\t$PAIRED\t$ST\t$G1\t$G2\t$E1\t$E2\t$F1\t$F2"
   fi
 done
